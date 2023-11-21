c50be2a18b9ad29337379ca614e9b880fca1b9c8
Tristan Bayol <tristan-bayol@laplateforme.io>
2023-11-21 09:04:14 +0100

    Ajout du Job pour aller plus loin

---
+++ Jour01/Pour aller plus loin/plusloin.py
@@ -0,0 +1,11 @@
+#Saisir une chaine de caractère !
+chaine = input("Entrez une chaine de caractère :")
+
+#Verifie si la chaine contient le caractère E
+#Si oui elle l'affiche
+if 'e' in chaine:
+    print("cette chaine de caractère contient un E")
+
+#Sinon !
+else: 
+    print("Cette chaine ne contient pas le caractère E")
